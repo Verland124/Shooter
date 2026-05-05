1b1c1e191c042d1b706e8ce6adadb5fe94d73e2f
Verland124 <VerlandRego124@gmail.com>
2026-05-05 18:48:20 +0700

    124

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,19 +4,48 @@ using UnityEngine;
 public class PlayerMove : NetworkBehaviour
 {
     public float speed = 5f;
+    private Animator animator;
 
+    void Start()
+    {
+        GetComponentInChildren<Animator>();
+        animator = GetComponent<Animator>();
+    }
     void Update()
     {
         // Только если это наш персонаж
         if (!IsOwner) return;
 
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        float inputHorizontal = Input.GetAxis("Horizontal");
+        float inputVertical = Input.GetAxis("Vertical");
 
-        if (x != 0 || z != 0)
+        if (inputHorizontal != 0 || inputVertical != 0)
         {
-            Vector3 move = new Vector3(x, 0, z).normalized * speed * Time.deltaTime;
+            Vector3 move = new Vector3(inputHorizontal, 0, inputVertical).normalized * speed * Time.deltaTime;
             transform.Translate(move);
         }
+        if (IsOwner)
+        {
+            Vector3 cameraForward = Camera.main.transform.forward;
+            cameraForward.y = 0;
+            cameraForward.Normalize();
+
+            Vector3 cameraRight = Camera.main.transform.right;
+            cameraRight.y = 0;
+            cameraRight.Normalize();
+
+            Vector3 moveDirection = (cameraForward * inputVertical + cameraRight * inputHorizontal).normalized;
+            if (moveDirection.magnitude > 0.1f)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
+                animator.SetFloat("Speed", 0.5f, 0.1f, Time.deltaTime);
+            }
+            else
+            {
+                animator.SetFloat("Speed", 0f, 0.1f, Time.deltaTime);
+            }
+        }
+
     }
 }
